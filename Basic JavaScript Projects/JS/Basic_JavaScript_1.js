b09325e9df3a9d8f120a4b405e8f82e55f971efc
ty0022 <38741629+ty0022@users.noreply.github.com>
2019-10-14 02:39:00 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,20 @@
+document.write("Hello, World! <br>"); 
+var A = "this is a string <br>";
+var A = A.fontcolor("red");
+document.write(A);
+var B = "this is a pop-up";
+var B = B.fontcolor("green");
+window.alert(B);
+document.write("My mom told me once, \"don't eat before you swim!\"<br>");
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss <br>");
+var C = "concatenated" + " String <br>"
+document.write(C)
+var Family = "The Robinsons", Dad = "Ty", Mom = "Kylee", Son = "Olyver <br>";
+var Fanily = Family.fontcolor("purple");
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("pink");
+var Son = Son.fontcolor("green");
+document.write(Son);
+document.write(3 + 3);
