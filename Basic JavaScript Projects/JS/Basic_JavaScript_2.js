347fcc5692be5e079b50659cf4588803030e3f2f
ty0022 <38741629+ty0022@users.noreply.github.com>
2019-10-20 21:29:23 -0700

    Add files via upload

--- Basic JavaScript Projects/JS/Basic_JavaScript_2.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -1,5 +1,10 @@
-function myFirstFunction()  {
+function my_First_Function()  {
     var str = "This text is green!";
     var result = str.fontcolor("green");
     document.getElementById("Green_Text").innerHTML = result;
+}
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book";
+    document.getElementById("concatenate").innerHTML = sentence;
 }
