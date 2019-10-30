f14e3e4801e2e0f3cabed3c7c20a4203763cc1f4
ty0022 <38741629+ty0022@users.noreply.github.com>
2019-10-30 02:04:01 -0700

    Add files via upload

--- Basic JavaScript Projects/JS/Basic_JavaScript_3.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -2,3 +2,39 @@ function addition_Function() {
     var addition = 2 + 2;
     document.getElementById("Math").innerHTML = "2 + 2 =" + addition ;
 }
+
+function subtraction_Function() {
+    var subtraction = 3 - 2;
+    document.getElementById("Math").innerHTML = "3 - 2 =" + subtraction ;
+}
+
+function multiplication() {
+    var simple_Math = 4 * 2;
+    document.getElementById("Math").innerHTML = "4 x 2 =" + simple_Math ;
+}
+
+function division() {
+    var simple_Math = 8 / 2;
+    document.getElementById("Math").innerHTML = "8 / 2 =" + simple_Math ;
+}
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Math").innerHTML = "(1 + 2) * 10 / 2 -5 =" + simple_Math
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math").innerHTML = "when you divide 25 by 6 you have a remainder of: " + simple_Math
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("Math").innerHTML = -x;
+}
+
+var x = 5;
+x++;
+document.write(x);
+
+window.alert(Math.random() * 100);
