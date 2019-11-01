26b597ade19b029fa154437652f51613b56bc8ef
ty0022 <38741629+ty0022@users.noreply.github.com>
2019-11-01 07:30:29 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_10.js
@@ -0,0 +1,47 @@
+function Call_Loop() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function array_function() {
+    var pics = [];
+    picd[0] = "pic1";
+    pics[1] = "pic2";
+    pics[2] = "pic3";
+    pics[3] = "pic4";
+    document.getElementById("Array").innerHTML = "this is a pic." + pic[3] + ".";
+}
+
+var X = 82;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X); 
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function cars() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
